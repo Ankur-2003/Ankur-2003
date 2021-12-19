ede99f5b17714004675c02d163e92f7eb56be725
Ankur-2003 <2020bteit00059@walchandsangli.ac.in>
2021-12-19 22:49:17 +0530

    Authentication System added

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('home', views.home, name='home'),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,22 +1,47 @@
-from django.shortcuts import render, HttpResponse
-from django.contrib.auth import authenticate
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import *
 
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        my_user = User.objects.create_user(username, email, password)
+        my_user.first_name = name
+        my_user.save()
+
     return render(request, 'signup.html')
 
+# Function to sign in the page.
 def signin(request):
-    if request.method == 'POST':
-        name = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('/home')
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            password = request.POST.get('password')
 
-        # Have used default django login system.
-        user = authenticate(username=name, password=password)
-        if user is not None:
-            allBooks = Books.objects.all()
-            print(allBooks)
-            context = {'allBooks':allBooks}
-            return render(request, 'home.html', context)
+            # Have used default django login system.
+            print(name, password)
+            user = authenticate(username=name, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/home')
+        return render(request, 'login.html')
 
+def home(request):
+    allBooks = Books.objects.all()
+    context = {'allBooks':allBooks}
+    return render(request, 'home.html', context)
+
+# Function to sign out from the page.
+def signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('/signin')
     return render(request, 'login.html')
